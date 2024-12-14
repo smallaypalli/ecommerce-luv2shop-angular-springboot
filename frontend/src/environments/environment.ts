e8e444a5682a5c500c12a4d11ac722dd46ff05c4
Anantha Naga <dasarisindhuja92@gmail.com>
2024-12-14 14:58:50 -0600

    ui

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8443/api',
+  apiUrl: 'https://https://backend-k-a-nagaraju1-dev.apps.rm1.0a51.p1.openshiftapps.com/api',
   stripePublishableKey:
     'pk_test_51NaiSfJ7CqAb5XkluTiXqbDsuJ6IMsWiZgTPCmmKaouA5D3UJGXpXZcFNFM1os4yTZonDreg6w9EUMkk2eagZAE600dy8Thabh',
 };
