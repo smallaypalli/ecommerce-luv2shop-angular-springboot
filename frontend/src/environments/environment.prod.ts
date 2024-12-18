2fa0ef4ed2e6b0dddd4ae1d3f147feba280de9c6
Anantha Naga <dasarisindhuja92@gmail.com>
2024-12-17 19:31:28 -0600

    upgradeanp

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://luv2shop-backend.onrender.com/api',
+  apiUrl: 'https://backend-k-a-nagaraju1-dev.apps.rm1.0a51.p1.openshiftapps.com/api',
   stripePublishableKey:
     'pk_test_51NaiSfJ7CqAb5XkluTiXqbDsuJ6IMsWiZgTPCmmKaouA5D3UJGXpXZcFNFM1os4yTZonDreg6w9EUMkk2eagZAE600dy8Thabh',
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://https://backend-k-a-nagaraju1-dev.apps.rm1.0a51.p1.openshiftapps.com/api',
+  apiUrl: 'https://backend-k-a-nagaraju1-dev.apps.rm1.0a51.p1.openshiftapps.com/api',
   stripePublishableKey:
     'pk_test_51NaiSfJ7CqAb5XkluTiXqbDsuJ6IMsWiZgTPCmmKaouA5D3UJGXpXZcFNFM1os4yTZonDreg6w9EUMkk2eagZAE600dy8Thabh',
 };
